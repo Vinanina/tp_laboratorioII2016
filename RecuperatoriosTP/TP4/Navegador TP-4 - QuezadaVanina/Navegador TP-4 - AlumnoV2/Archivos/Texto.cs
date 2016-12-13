4415680d5591ad2ea4cee4a9adc97d7da9418ed9
Vinanina <vaninaquezada@gmail.com>
2016-12-13 03:48:24 -0300

    TP4

---
+++ RecuperatoriosTP/TP4/Navegador TP-4 - QuezadaVanina/Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+       protected string archivo;
+
+        public Texto(string archivo)
+        {
+            this.archivo = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try{
+                using (StreamWriter writer = new StreamWriter(this.archivo))
+                {
+                    writer.WriteLine(datos);
+                }
+
+                return true;
+            }catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            try {
+
+                using (StreamReader reader = new StreamReader(this.archivo))
+                    {
+                        List<string> lineas = new List<string>();
+
+                        while (!(reader.ReadLine() == null))
+                        {
+                            lineas.Add(reader.ReadLine());
+                        }
+
+                        datos = lineas;
+                    }
+                        return true;
+               }
+            catch(Exception e)
+            {
+                  throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP4/Navegador TP-4 - QuezadaVanina/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+
+
+        public delegate void EventProgreso(int progress);
+        public event EventProgreso Progreso;
+        public delegate void EventCompletado(string url);
+        public event EventCompletado Completo;
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += this.WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += this.WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }            
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+            this.Progreso(e.ProgressPercentage);
+
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (!e.Cancelled && e.Error == null)
+            {
+                this.html = (string)e.Result;
+            }
+            this.Completo(this.html);
+
+        }
+
+       // public DownloadProgressChangedEventHandler DownloadProgressCallback { get; set; }
+    }
+}
